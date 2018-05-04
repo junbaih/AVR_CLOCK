7f0bea8832581959927599b837aeb751ac3aeeff
Junbai Hou <junbaih@gmail.com>
2018-05-04 01:08:03 -0700

    fixed leap year

--- main.c
+++ main.c
@@ -4,8 +4,32 @@
 #include "lcd.c"
 #include "avr.h"
 #include "avr.c"
-#define DATE_R 0
-#define TIME_R 1
+#define A 10
+#define B 11
+#define C 12 
+#define D 13
+#define STAR 14
+#define Hash 15
+#define None 255
+
+struct time
+{
+	unsigned short hour;
+	unsigned short minute;
+	unsigned short second;
+	unsigned char time_mode;
+	unsigned char aORp;
+} lcd_time;
+
+
+struct date
+{
+	unsigned short year;
+	unsigned short  day;
+	unsigned short  month;
+} lcd_date;
+
+
 unsigned char is_pressed(unsigned char r, unsigned char c)
 {
 	DDRC = 0;
@@ -24,63 +48,72 @@ unsigned char is_pressed(unsigned char r, unsigned char c)
 }
  unsigned char get_key()
 {
-	unsigned int temp;
+	unsigned char temp;
 	for(unsigned char r = 0;r<4;r++)
 	for(unsigned char c=0;c<4;c++)
 	{
 		if(is_pressed(r,c))
 		{
 			temp = 4*r+c+1;
-		if(temp<8 && temp>4)
-			temp--;
-		if(temp>8 && temp<12)
-			temp-=2;
-		if(temp == 14)
-			temp = 0;
-		else{
+
 		switch(temp)
 		{
+			case 1:
+				temp = 1;
+				break;
+			case 2:
+				temp =2;
+				break;
+			case 3:
+				temp =3;
+				break;
 			case 4:
-				temp = 10;
+				temp = A;
+				break;
+			case 5:
+				temp =4;
+				break;
+			case 6:
+				temp =5;
+				break;
+			case 7:
+				temp = 6;
 				break;
 			case 8:
-				temp = 11;
+				temp = B;
 				break;
+			case 9:
+				temp = 7;
+				break;
+			case 10:
+				temp = 8;
+				break;
+			case 11:
+				temp = 9;
+				break;	
 			case 12:
-				temp = 12;
+				temp = C;
 				break;
 			case 16:
-				temp  =13;
+				temp  =D;
 				break;
 			case 13:
-				temp = 14;
+				temp = STAR;
 				break;
 			case 15:
-				temp = 15;
+				temp = Hash;
+				break;
+			case 14:
+				temp = 0;
 				break;
 			default:
 				break;	
 		}
+		return temp;
 		}
-		//while(is_pressed(r,c));
-		return temp;}
 	}
-	return 124;
+	return None;
 }
-struct time
-{
-	unsigned char hour;
-	unsigned char minute;
-	unsigned char second;
-} lcd_time;
-
-
-struct date
-{
-	unsigned short year;
-	unsigned char  day;
-	unsigned char  month;
-} lcd_date;
 
 
 void display(){
@@ -91,61 +124,38 @@ void display(){
 	sprintf(date_buffer, "%02d/%02d/%04d",lcd_date.month,lcd_date.day,lcd_date.year);
 	pos_lcd(0,0);
 	puts_lcd2(date_buffer);
-
+	//char AP[2] = {'P','M'};
 	sprintf(time_buffer, "%02d:%02d:%02d",lcd_time.hour,lcd_time.minute,lcd_time.second);
 	pos_lcd(1,0);
 	puts_lcd2(time_buffer);
 }
-unsigned char is_num_key(unsigned char k)
-{
-	if( k>=0 && k<=9)
-		return 1;
-	return 0;
-}
-unsigned char setting(){
-	unsigned char k = 0;
-	unsigned char j = 0;
-	while (1)
-	{
-		k = get_key();
-		if (k==10)
-			return 66;
-		if(is_num_key(k))
-		{	
-			while(1)
-			{
-				j = get_key();
-				if(is_num_key(j))
-					return 10*k+j; 
-			}
-		}
-	}
-
-}
 
 
 
-void editing_mode(unsigned char* set_mode)
+
+
+void editing_mode(unsigned short* set_mode)
 {
 	while(1)
 	{
 		char key = get_key();
 		switch (key)
 		{
-			case 10:
+			case A:
 			(*set_mode)++;
 			display();
-			wait_avr(1000);
+			wait_avr(300);
 			break;
 			
-			case 11:
-						(*set_mode)--;
-						display();
-						wait_avr(1000);	
-						break;
+			case B:
+			(*set_mode)--;
+			display();
+			wait_avr(300);	
+		    break;
 					
 			
-			case 12:
+			case C:
+			//exit
 			return;
 			
 			default:
@@ -155,45 +165,25 @@ void editing_mode(unsigned char* set_mode)
 	
 	}
 }
-
-
-unsigned short setting_short(){
-	unsigned char k = 0;
-	unsigned char j = 0;
-	unsigned char i = 0;
-	unsigned char h = 0;
-	while(k == 0)
-	k = get_key();
-	if(k == 10)
-	return 0;
-	while(j == 0)
-	j = get_key();
-	if(j == 10)
-	return 0;
-	while(i == 0)
-	i = get_key();
-	if(i == 10)
-	return 0;
-	while(h == 0)
-	h = get_key();
-	if(h == 10)
-	return 0;
-	else
-	return k*1000 + j*100 + i*10 + h;
+void time_M2R()
+{
+	lcd_time.aORp = lcd_time.hour>12?0:1; 
+	lcd_time.hour=lcd_time.hour%12;
+	lcd_time.time_mode=1;
 }
-
-
-
-
-
-
-
-
-
-void enter_ui(){
-	edit_month();
+void time_R2M()
+{
+	if(!lcd_time.aORp)
+		lcd_time.hour+=12;
+	lcd_time.time_mode=0;
 }
-
+void time_mode_switch()
+{
+	if(lcd_time.time_mode)
+		time_R2M();
+	time_M2R();
+	
+}
 
 unsigned char check_month(){
 	unsigned char d = lcd_date.day;
@@ -216,22 +206,33 @@ unsigned char check_month(){
 			return 1;
 		}
 	}
-	if(d > 28){
-		if(m == 2){
-			if((y%4 == 0 && y%100 != 0) 
-				|| y%400 == 0){
-				if(d > 29){
+	if(d > 28)
+	{
+		if(m == 2)
+		{
+			if((y%4 == 0 && y%100 != 0) || y%400 == 0)
+			{
+				if(d > 29)
+				{
 					return 1;
 				}
 			}
-			else{
-				return 1;
-			}
+
+			return 0;
+		
 		}
 	}
 	return 0;
 }
 
+char leap()
+{
+	
+	unsigned short y = lcd_date.year;
+	if((!(y%4) && (y%100))|| !(y%400))
+	return 1;
+	return 0;
+}
 void self_run(){
     lcd_time.second += 1;
     if (lcd_time.second >= 60)
@@ -248,11 +249,48 @@ void self_run(){
     {
     	lcd_time.hour = 0;
     	lcd_date.day += 1;
-    	if(check_month()){
+    /*	if(check_month()){
             lcd_date.day = 1;
 		    lcd_date.month += 1;
-    	}
+    	}*/
     }
+	if(lcd_date.day>28)
+	{
+		if(lcd_date.month==2)
+		{
+			if(!leap(lcd_date.year))
+				{
+					lcd_date.day=1;
+					lcd_date.month++;
+				}
+			else if(lcd_date.month>29)
+			{
+					lcd_date.day=1;
+					lcd_date.month++;
+			}
+			
+		}
+		else
+		{
+			if(lcd_date.month == 4 || lcd_date.month == 6 ||lcd_date.month == 9||lcd_date.month == 11)
+			{
+				if(lcd_date.day>30)
+				{
+					lcd_date.day=1;
+					lcd_date.month++;
+				}
+			}
+			else
+			{
+				if(lcd_date.day>31)
+				{
+					lcd_date.day=1;
+					lcd_date.month++;
+				}
+				
+			}
+		}
+	}
     if(lcd_date.month > 12){
     	lcd_date.month = 1;
     	lcd_date.year += 1;
@@ -260,26 +298,52 @@ void self_run(){
 }
 
 
+
+
 /* ----------------------------------------------- */
-void set_hour()
-{
-	//if get_key()
-	 
-	 //nt 
-}
+
 void init_clock()
 {
 		lcd_time.minute = 0;
 		lcd_time.second=0;
 		lcd_time.hour=0;	
+		lcd_time.time_mode=0;
 	    lcd_date.day = 1;
 	    lcd_date.month = 1;
 	    lcd_date.year = 2018;
 	
 }
 
+void sim1()
+{
+	lcd_time.minute = 59;
+	lcd_time.second = 50;
+	lcd_time.hour = 23;
+	lcd_date.year=2008;
+	lcd_date.month=2;
+	lcd_date.day=28;
+}
 
+void sim2()
+{
+	lcd_time.minute = 59;
+	lcd_time.second = 50;
+	lcd_time.hour = 23;
+	lcd_date.year=2000;
+	lcd_date.month=2;
+	lcd_date.day=28;
+}
 
+void sim3()
+{
+		lcd_time.minute = 59;
+		lcd_time.second = 50;
+		lcd_time.hour = 23;
+		lcd_date.year=1900;
+		lcd_date.month=2;
+		lcd_date.day=28;
+
+}
 
 
 int main(void)
@@ -297,48 +361,57 @@ int main(void)
     	input = get_key();/*check the input from keyboard*/
     
     		self_run();/*run clock*/
-//unsigned char* m = &(lcd_date.month);
+
 			switch(input)
 			{
-				case 13:
+				case D:
 				init_clock();
 				break;
 			
     	//}		
-		case 1:
+				case 1:
 			// EDIT
 			
-			editing_mode(&(lcd_date.month));
-			break;
+				editing_mode(&(lcd_date.month));
+				break;
 			
-		case 2:
-			editing_mode(&(lcd_date.day));
+				case 2:
+				editing_mode(&(lcd_date.day));
+				break;
+				case 3:
+				editing_mode(&(lcd_date.year));
+				break;	 		
+				case 4:
+				editing_mode(&(lcd_time.hour));
+				break;
+				
+				case 5:
+				editing_mode(&(lcd_time.minute));
+				break;
+				
+				case STAR:
+				sim1();
+				display();
+				break;
+				/*
+				time_mode_switch();
+				display();
+				break;
+				
+				
+*/
+			case Hash:
+			sim2();
+			display();
 			break;
-		/*case 3:
-			editing_mode(&(lcd_date.year));
-			break;	*/		
-		case 4:
-					editing_mode(&(lcd_time.hour));
-					break;
-	    case 5:
-					editing_mode(&(lcd_time.minute));
-					break;
-		case 11:
+			case 0:
+				sim3();
+				display();
+				break;
+			default:
 			break;
-			
-		default:
-		break;
 		}
-		/*
-11111111111111		clr_lcd();
-		pos_lcd(0,0);
-    	sprintf(date_buffer, "%02d/%02d/%4d",lcd_date.month,lcd_date.day,lcd_date.year);
-		puts_lcd2(date_buffer);
-		
-		pos_lcd(1,0);
-		sprintf(time_buffer, "%02d:%02d:%02d",lcd_time.hour,lcd_time.minute,lcd_time.second);
 
-    	wait_avr(900);*/
 		display();
 		wait_avr(900);
    
